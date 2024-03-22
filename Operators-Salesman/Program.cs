accdf6b315cee0177e5996c2d192f2d90ddb4a9a
DevilishSasuke <108042884+DevilishSasuke@users.noreply.github.com>
2024-03-23 00:57:59 +0300

    init commit, variant 40

--- Operators-Salesman/Program.cs
+++ Operators-Salesman/Program.cs
@@ -1,4 +1,20 @@
 ﻿namespace Operators
 {
+    class Program
+    {
+        public static void Main()
+        {
+            decimal[][] matrix =
+            {
+                new decimal[]{ 0, 7, 8, 8, 9, 5},
+                new decimal[]{ 3, 0, 6, 5, 8, 4},
+                new decimal[]{ 1, 5, 0, 4, 7, 4},
+                new decimal[]{ 6, 6, 1, 0, 8, 1},
+                new decimal[]{ 6, 8, 9, 7, 0, 3},
+                new decimal[]{ 7, 3, 9, 8, 7, 0}
+            };
 
+            var salesman = new TravellingSalesman(matrix);
+        }
+    }
 }
---
+++ Operators-Salesman/TravellingSalesman.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Operators
+{
+    public class TravellingSalesman
+    {
+        public decimal[][] Paths { get; private set; }
+        public TravellingSalesman(decimal[][] paths)
+        {
+            int width = paths.Length;
+            foreach (var path in paths)
+                if (path.Length != width)
+                    throw new Exception("Matrix is not square");
+
+            Paths = paths;
+        }
+    }
+}
